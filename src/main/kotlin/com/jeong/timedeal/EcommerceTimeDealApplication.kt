c00df7162ee2c766711dc1cd40e53ca4ebd020b5
seohyun jeong <jeongseohyun31@gmail.com>
2023-03-07 13:28:07 +0900

    init project

---
+++ src/main/kotlin/com/jeong/timedeal/EcommerceTimeDealApplication.kt
@@ -0,0 +1,11 @@
+package com.jeong.timedeal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceTimeDealApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommerceTimeDealApplication>(*args)
+}
---
+++ src/test/kotlin/com/jeong/timedeal/EcommerceTimeDealApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jeong.timedeal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceTimeDealApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
